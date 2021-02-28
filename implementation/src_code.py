b1b5ed377abe5d1e541482d38842c290869ad25b
Aiswarya PS <69056143+AiswaryaPS@users.noreply.github.com>
2021-03-01 00:44:44 +0530

    Update src_code.py

--- implementation/src_code.py
+++ implementation/src_code.py
@@ -2,22 +2,16 @@ import os
 import csv
 import pandas as pd
 from functools import reduce
-
 desktop = os.path.join(os.path.expanduser("~"), "Desktop")
 filePath = os.path.join(desktop, "ScriptTestFolder", "InputFolder")
 outfilePath = os.path.join(desktop, "ScriptTestFolder")
-
 dfList = []
 list_of_rows = [list(row) for row in df.values]
-# print(df)
 f = open("results/studentMarks.csv", "w")
 f.write("ID,NAME,EMAIL,Module1,Module2,Module3,Average\n")
 for i in list_of_rows:
-  # print(i)
   for j in i:
-    # print(j)
     f.write(str(j).strip()+",")
-
   mod1= pd.read_csv('InputFolder/Module1.csv'', delimiter=',')  
   if i[0] in subPhy.values:
     temp_rows= [list(row) for row in mod1.values]
@@ -28,7 +22,6 @@ for i in list_of_rows:
     f.write(str(mod1.values[t][3])+",")
   else: 
     f.write("NA,")
-
   mod2 = pd.read_csv('InputFolder/Module2.csv', delimiter=',')  
   if i[0] in mod2.values:
     temp_rows= [list(row) for row in mod2.values]
@@ -39,7 +32,6 @@ for i in list_of_rows:
     f.write(str(mod2.values[t][3])+",")
   else:
     f.write("NA,")
-
   mod3 = pd.read_csv('InputFolder/Module3.csv', delimiter=',')  
   if i[0] in mod3.values:
     temp_rows= [list(row) for row in mod3.values]
@@ -49,8 +41,7 @@ for i in list_of_rows:
         break
     f.write(str(mod3.values[t][3])+",")
   else:
-    f.write("NA,")
-  
+    f.write("NA,") 
   mod4 = pd.read_csv('InputFolder/Module4.csv', delimiter=',')  
   if i[0] in mod4.values:
     temp_rows= [list(row) for row in mod4.values]
@@ -60,12 +51,9 @@ for i in list_of_rows:
         break
     f.write(str(mod4.values[t][3])+",")
   else:
-    f.write("NA,")
-    
+    f.write("NA,")  
   f.write("\n")
 f.close()
-
-
 for file in os.listdir(filePath):
     #print(file)
     filepath = os.path.join(desktop, filePath, file)
@@ -74,8 +62,6 @@ for file in os.listdir(filePath):
 
 mergedDF= df = pd.concat(dfList).reset_index()
 mergedDF.to_csv(os.path.join(outfilePath, "Output.csv"))
-
 print(mergedDF.Name.nunique())
 
 
-#THIS IS A CHANGE... A BORING CHANGE>> BUT A CHANGE
