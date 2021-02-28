55b28ba5869b92b55952672cfd51a4fb99d0677c
Theres Mary <theres.mec@gmail.com>
2021-03-01 00:28:56 +0530

    modified

--- implementation/src_code.py
+++ implementation/src_code.py
@@ -8,6 +8,80 @@ filePath = os.path.join(desktop, "ScriptTestFolder", "InputFolder")
 outfilePath = os.path.join(desktop, "ScriptTestFolder")
 
 dfList = []
+
+# Create a dataframe from csv
+df = pd.read_csv('InputFolder/Module1.csv',)
+# User list comprehension to create a list of lists from Dataframe rows
+list_of_rows = [list(row) for row in df.values]
+# print(df)
+f = open("results/studentMarks.csv", "w")
+f.write("studentID,studentNAME,studentEMAIL,Module1,Module2,Module3,Average\n")
+for i in list_of_rows:
+  # print(i)
+  for j in i:
+    # print(j)
+    f.write(str(j).strip()+",")
+
+  mod1= pd.read_csv('InputFolder/Module1.csv'', delimiter=',')  
+  if i[0] in subPhy.values:
+    temp_rows= [list(row) for row in mod1.values]
+    for row in temp_rows:
+      if i[0] in row:
+        t=temp_rows.index(row)
+        break
+    f.write(str(mod1.values[t][3])+",")
+  else: 
+    f.write("NA,")
+
+  subChe = pd.read_csv('dataset/Mod.csv', delimiter=',')  
+  if i[0] in subChe.values:
+    temp_rows= [list(row) for row in subChe.values]
+    for row in temp_rows:
+      if i[0] in row:
+        t=temp_rows.index(row)
+        break
+    f.write(str(subChe.values[t][3])+",")
+  else:
+    f.write("NA,")
+
+  subBio = pd.read_csv('dataset/Biology.csv', delimiter=',')  
+  if i[0] in subBio.values:
+    temp_rows= [list(row) for row in subBio.values]
+    for row in temp_rows:
+      if i[0] in row:
+        t=temp_rows.index(row)
+        break
+    f.write(str(subBio.values[t][3])+",")
+  else:
+    f.write("NA,")
+  
+  subMat = pd.read_csv('dataset/Maths.csv', delimiter=',')  
+  if i[0] in subMat.values:
+    temp_rows= [list(row) for row in subMat.values]
+    for row in temp_rows:
+      if i[0] in row:
+        t=temp_rows.index(row)
+        break
+    f.write(str(subMat.values[t][3])+",")
+  else:
+    f.write("NA,")
+
+
+  subPy = pd.read_csv('dataset/Python.csv', delimiter=',')  
+  if i[0] in subPy.values:
+    temp_rows= [list(row) for row in subPy.values]
+    for row in temp_rows:
+      if i[0] in row:
+        t=temp_rows.index(row)
+        break
+    f.write(str(subPy.values[t][3])+",")
+  else:
+    f.write("NA,")
+    
+  f.write("\n")
+f.close()
+
+
 for file in os.listdir(filePath):
     #print(file)
     filepath = os.path.join(desktop, filePath, file)
